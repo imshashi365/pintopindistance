08fdd1f8ac9811a1c177686be7ac3565f25b064a
DurgaRani8115 <95562773+imshashi365@users.noreply.github.com>
2025-07-20 09:52:13 +0530

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.pin-to-pin-distance.xyz'
+  const baseUrl = 'https://pin-to-pin-distance.xyz'
   
   const blogPosts = [
     {
